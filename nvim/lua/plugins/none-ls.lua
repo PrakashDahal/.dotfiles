5f7d9e94dca8be8cdea72a8b1e0a9bc9b6aaaa18
prakashg7 <dahalprakash1720@gmail.com>
2024-05-05 08:58:55 +0545

    Fix minor issues

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,12 +1,16 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
+        require("none-ls.diagnostics.eslint_d"),
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
+        -- null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.completion.spell,
 
       },
