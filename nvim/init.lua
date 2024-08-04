9e926b19d2aaeaffe28b2d43894799b8e15ae0bb
prakashg7 <dahalprakash1720@gmail.com>
2024-08-04 10:17:30 +0545

    Add ai tool to enhance efficiency: tabnine

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,33 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-config")
-require("lazy").setup({{import = "plugins" }, {import = "plugins.lsp" }},{
-  change_detection = {
-    notify = false,
-  },
+require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
+	change_detection = {
+		notify = false,
+	},
+})
+
+require("tabnine").setup({
+	max_lines = 1000,
+	max_filesize = 5000,
+	disable_auto_comment = true,
+	accept_keymap = "<Tab>",
+	dismiss_keymap = "<C-]>",
+	debounce_ms = 100,
+	suggestion_color = { gui = "#808080", cterm = 244 },
+	exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+	log_file_path = nil, -- absolute path to Tabnine log file
+	ignore_certificate_errors = false,
+	max_number_results = 20,
 })
---
+++ nvim/lua/plugins/ai-tool.lua
@@ -0,0 +1,5 @@
+return {
+	"codota/tabnine-nvim",
+	event = "InsertEnter",
+	build = "./dl_binaries.sh",
+}
